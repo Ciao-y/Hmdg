e7b4b7ee6fc139b8faf5acbfb223de2edad8ca82
huan <2535644315@qq.com>
2022-10-27 04:34:24 +0800

    完成搜索页面

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,46 +1,50 @@
 <template>
-  <view class="scroll-view-container">
-    <!-- 左侧的滚动视图区域 -->
-    <scroll-view
-      class="left-scroll-view"
-      scroll-y
-      :style="{ height: wh + 'px' }"
-    >
-      <block v-for="(item, index) in cateList" :key="index">
-        <view
-          :class="['left-scroll-view-item', index === active ? 'active' : '']"
-          @click="activeChanged(index)"
-          >{{ item.cat_name }}</view
-        >
-      </block>
-    </scroll-view>
-
-    <!-- 右侧的滚动视图区域 -->
-    <scroll-view
-      class="right-scroll-view"
-      scroll-y
-      :style="{ height: wh + 'px' }"
-      :scroll-top="scrollTop"
-    >
-      <view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
-        <view class="cate-lv2-title">/ {{ item2.cat_name }} /</view>
-        <!-- 动态渲染三级分类的列表数据 -->
-        <view class="cate-lv3-list">
-          <!-- 三级分类 Item 项 -->
+  <view>
+    <!-- 搜索栏 -->
+    <my-search @click="gotoSearch">></my-search>
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动视图区域 -->
+      <scroll-view
+        class="left-scroll-view"
+        scroll-y
+        :style="{ height: wh + 'px' }"
+      >
+        <block v-for="(item, index) in cateList" :key="index">
           <view
-            class="cate-lv3-item"
-            v-for="(item3, i3) in item2.children"
-            :key="i3"
-            @click="gotoGoodsList(item3)"
+            :class="['left-scroll-view-item', index === active ? 'active' : '']"
+            @click="activeChanged(index)"
+            >{{ item.cat_name }}</view
           >
-            <!-- 图片 -->
-            <image :src="item3.cat_icon"></image>
-            <!-- 文本 -->
-            <text>{{ item3.cat_name }}</text>
+        </block>
+      </scroll-view>
+
+      <!-- 右侧的滚动视图区域 -->
+      <scroll-view
+        class="right-scroll-view"
+        scroll-y
+        :style="{ height: wh + 'px' }"
+        :scroll-top="scrollTop"
+      >
+        <view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+          <view class="cate-lv2-title">/ {{ item2.cat_name }} /</view>
+          <!-- 动态渲染三级分类的列表数据 -->
+          <view class="cate-lv3-list">
+            <!-- 三级分类 Item 项 -->
+            <view
+              class="cate-lv3-item"
+              v-for="(item3, i3) in item2.children"
+              :key="i3"
+              @click="gotoGoodsList(item3)"
+            >
+              <!-- 图片 -->
+              <image :src="item3.cat_icon"></image>
+              <!-- 文本 -->
+              <text>{{ item3.cat_name }}</text>
+            </view>
           </view>
         </view>
-      </view>
-    </scroll-view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -59,7 +63,7 @@ export default {
   },
   onLoad ( options ) {
     const sysInfo = uni.getSystemInfoSync()
-    this.wh = sysInfo.windowHeight;
+    this.wh = sysInfo.windowHeight - 50;
     this.getcateList()
   },
   methods: {
@@ -79,6 +83,9 @@ export default {
       uni.navigateTo( {
         url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
       } )
+    },
+    gotoSearch () {
+      uni.navigateTo( { url: '/subpkg/search/search' } )
     }
   },
 }
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
   <view>
+    <!-- 搜索栏 -->
+    <view class="search-box">
+      <my-search @click="gotoSearch"></my-search>
+    </view>
     <!-- 轮播图区域 -->
     <swiper
       :indicator-dots="true"
@@ -21,7 +25,12 @@
     </swiper>
     <!-- 分类导航区域 -->
     <view class="nav-list">
-      <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+      <view
+        class="nav-item"
+        v-for="(item, i) in navList"
+        :key="i"
+        @click="navClickHandler(item)"
+      >
         <image :src="item.image_src" class="nav-img"></image>
       </view>
     </view>
@@ -64,101 +73,112 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       swiperList: [],
       navList: [],
       FloorList: [],
     }
   },
-  onLoad() {
+  onLoad () {
     this.getSwiperList()
     this.getNavList()
     this.getFloorList()
   },
   methods: {
     //获取轮播图数据的方法
-    async getSwiperList() {
+    async getSwiperList () {
       // 3.1 发起请求
-      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+      const { data: res } = await uni.$http.get( '/api/public/v1/home/swiperdata' )
       // 3.2 请求失败
-      if (res.meta.status !== 200) {
-        return uni.showToast({
+      if ( res.meta.status !== 200 ) {
+        return uni.showToast( {
           title: '数据请求失败！',
           duration: 1500,
           icon: 'none',
-        })
+        } )
       }
       // 3.3 请求成功，为 data 中的数据赋值
       this.swiperList = res.message
-      uni.$showMsg('数据加载成功')
+      uni.$showMsg( '数据加载成功' )
     },
     //分类导航的数据列表
-    async getNavList() {
-      const { data: res } = await uni.$http.get('/api/public/v1/home/catitems')
-      if (res.meta.status !== 200) return uni.$showMsg()
+    async getNavList () {
+      const { data: res } = await uni.$http.get( '/api/public/v1/home/catitems' )
+      if ( res.meta.status !== 200 ) return uni.$showMsg()
 
       this.navList = res.message
     },
     // 点击分类导航跳转该页面
-    navClickHandler(item) {
-      if (item.name === '分类') {
-        uni.switchTab({
+    navClickHandler ( item ) {
+      if ( item.name === '分类' ) {
+        uni.switchTab( {
           url: '/pages/cate/cate',
-        })
+        } )
       }
     },
     // 获取图片列表
-    async getFloorList() {
-      const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
-      if (res.meta.status !== 200) return uni.$showMsg()
-      res.message.forEach((floor) => {
-        floor.product_list.forEach((prod) => {
-          prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
-        })
-      })
+    async getFloorList () {
+      const { data: res } = await uni.$http.get( '/api/public/v1/home/floordata' )
+      if ( res.meta.status !== 200 ) return uni.$showMsg()
+      res.message.forEach( ( floor ) => {
+        floor.product_list.forEach( ( prod ) => {
+          prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split( '?' )[ 1 ]
+        } )
+      } )
       this.FloorList = res.message
     },
+    gotoSearch () {
+      uni.navigateTo( {
+        url: '/subpkg/search/search'
+      } )
+    }
+
   },
 }
 </script>
 
 <style lang="scss">
-swiper {
-  height: 330rpx;
+  swiper {
+    height: 330rpx;
 
-  .swiper-item,
-  image {
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+
+  .floor-title {
+    height: 60rpx;
     width: 100%;
-    height: 100%;
+    display: flex;
   }
-}
 
-.nav-list {
-  display: flex;
-  justify-content: space-around;
-  margin: 15px 0;
-
-  .nav-img {
-    width: 128rpx;
-    height: 140rpx;
+  .right-img-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
   }
-}
 
-.floor-title {
-  height: 60rpx;
-  width: 100%;
-  display: flex;
-}
-
-.right-img-box {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-}
-
-.floor-img-box {
-  display: flex;
-  padding-left: 10rpx;
-}
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
+  }
+  .search-box {
+    position: sticky;
+    top: 0;
+    z-index: 999;
+  }
 </style>
